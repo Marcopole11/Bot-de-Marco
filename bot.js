697e5c3adf4d3cbbd53ef7d555fd336beb19ff56
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-22 13:46:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,5 +15,10 @@ client.on('message', message => {
         message.guild.channels.find("name", "canal-r37j").sendMessage('pong');
     }
 });
+client.on('message', message => {
+    if (message.content.startsWith(prefix + 'noticeame')) {
+        message.member.addRole(guild.roles.find("name", "ad");
+    }
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
