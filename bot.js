4987f34ab553c41a227b6687c7c92ef7c43a85fe
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-22 13:04:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,17 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
+var prefix = 'm!';
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-  	}
+  	} else if (message.content.startsWith(prefix + 'ping')) {
+    	message.channel.sendMessage('pong');
+    } else if (message.content.startsWith(prefix + 'testcomando')) {
+        message.guild.channels.find("name", "canal-r37j");
+    } else if (message.content.startsWith(prefix + 'noticeame')) {
+        message.member.addRole(guild.roles.find("name", "ad");
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
