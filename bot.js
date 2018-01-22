b6f1986b1450dca148a0043c6e014e0d1a16c1e7
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-22 13:53:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content.startsWith(prefix + 'noticeame')) {
-        message.member.addRole(message.guild.roles.find("name", "ad");
+        message.member.addRole(message.guild.roles.find("name", "ad"), "comando");
     }
 });
 // THIS  MUST  BE  THIS  WAY
