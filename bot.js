87cacad268cc9f32c6258503e97fa4b8fa949edf
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-24 10:34:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,10 +48,10 @@ client.on('message', message => {
                 envio = (envio + "\n" + entrada[lain]);
             }
         }
+        message.guild.channels.find("name", "chatprincipal").sendMessage(envio);
         for(lain = 0; lain < opciones; lain++){
             message.client.user.lastMessage.react(message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", emotinames[lain]));
         }
-        message.guild.channels.find("name", "chatprincipal").sendMessage(envio);
     }
 });
 client.on('message', message => {
