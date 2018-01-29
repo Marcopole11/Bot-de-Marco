f1ecd571d177af8c073a2cdf5c39e72ced160d4c
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-30 00:26:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,7 +106,9 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if ((message.author.username == "MuxyBot" || message.author.username == "Skuld") && message.channel.name == dialogchat) {
-        message.channel.sendMessage('tom!endirecto');
+        if(message.content.startsWith('@everyone marcopole just went live!'){
+           message.channel.sendMessage('tom!endirecto');
+        }
     } else if (message.content.startsWith(prefix + 'endirecto') && message.channel.name == dialogchat) {
         let entrada = message.content.split(" ARTEIKA ");
         let salida = message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
