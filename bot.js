7c80bc99c9a252f080dae6a669b0a33444d3519c
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-29 20:16:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if ((message.author.username == "MuxyBot" || message.author.username == "Skuld") && message.channel.name == dialogchat) {
-        message.channel.sendMessage('!drawing');
+        message.channel.sendMessage('tom!endirecto');
     } else if (message.content.startsWith(prefix + 'endirecto') && message.channel.name == dialogchat) {
         let entrada = message.content.split(" ARTEIKA ");
         let salida = salida + message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
