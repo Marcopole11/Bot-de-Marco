fe7d4f7516c426355bb34c66364abcb1d4da7f23
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-29 10:25:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,6 +69,11 @@ client.on('message', message => {
         });
     }
 });
+client.on('message', message => {
+    if (message.content.startsWith(prefix + 'nuevodirecto') && message.channel.name == dialogchat) {
+        message.guild.channels.find("name", "aviso-directos").sendMessage('prueba');
+    }
+}
 
 /*
         let entrada = message.content.split("ARTEIKA");
