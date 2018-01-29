67ff4799cbc2f7aa446fcdd28623f2a803508fbf
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-29 10:22:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,12 +71,12 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content.startsWith(prefix + 'nuevodirecto') && message.channel.name == dialogchat) {
-        let entrada = message.content.split("ARTEIKA");
-        let salida = "prueba";
-        message.guild.channels.find("name", "aviso-directos").sendMessage(salida);
+        message.guild.channels.find("name", "aviso-directos").sendMessage('prueba');
     }
 }
 /*
+        let entrada = message.content.split("ARTEIKA");
+        let salida = "prueba";
 
 kap[("$(3)".charCodeAt(0)+ "").slice(-1)];)
 client.on('message', message => {
