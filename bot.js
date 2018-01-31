8ac712a481c5cbfdc75ee9ebedb2d3b71d76282a
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-01-31 09:47:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,6 +65,8 @@ client.on('message', message => {
         }
         envio = ("Fastoll indefinido " + opciones + " RekTeaQuerry\n" + envio);
         message.guild.channels.find("name", "chatprincipal").sendMessage(envio);
+    } else if (message.author.username == message.client.user.username){
+        message.channel.sendMessage('Detectado');
     }
 });
 client.on('message', message => {
