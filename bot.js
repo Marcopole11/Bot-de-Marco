55597b870073b1ebf50595d0f28970565eee005d
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-02-20 10:35:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -148,9 +148,9 @@ client.on("guildMemberAdd", (member) => {
     member.addRole(member.guild.roles.find("name", "newbye"), "Por seguridad");
     member.addRole(member.guild.roles.find("name", "ad"), "Recien llegado");
     if(member.username.includes("a")){
-        guild.channels.find("name", "recibimiento").sendMessage("tiene a");
+        //guild.channels.find("name", "recibimiento").sendMessage("tiene a");
     } else {
-        guild.channels.find("name", "recibimientos").sendMessage("no tiene a");
+        //guild.channels.find("name", "recibimientos").sendMessage("no tiene a");
     }
 });
 /*
