299cd95681e205486a6683d61650987aa841edd5
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-02-22 13:15:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,6 +95,13 @@ client.on('message', message => {
     } else if (message.content == 'spec'){
         message.channel.sendMessage("See or Change?");
         const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        collector.on('collect', message => {
+            if (message.content == "See") {
+                message.channel.send("You Want To See Someones Spec OK!");
+            } else if (message.content == "Change") {
+                message.channel.send("You Want To Change Your Spec OK!");
+            }
+        }
         message.channel.sendMessage('Soy fuerte!');
     }
 });
