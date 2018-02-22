c917c9332b3ce38d2220222bcbd4235179baa102
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-02-22 13:00:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ client.on('message', message => {
         let entrada = message.content.split("RekTeaQuerry");
         message.edit(entrada[1]);
         message.react(message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "Oland_flag"));
-    } else if (command === 'spec'){
+    } else if (message.content == 'spec'){
         message.channel.sendMessage('Soy fuerte!');
     }
 });
