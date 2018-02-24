78f8f2edad41cc616981689b2daa2b4bec3f97fd
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-02-24 11:34:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,7 @@ client.on('messageUpdate', (omsg, nmsg) =>  {
         let entrada = nmsg.content.split(" ");
         if(entrada[1] == ":gun:" && entrada[2] == ":skull:¡Has" && entrada[3] == "muerto!:skull:,"){
               //omsg.guild.channels.find("name", "log").sendMessage(entrada[0].joinedTimestamp);
-             omsg.guild.channels.find("name", "log").sendMessage("yep");
+             omsg.guild.channels.find("name", "log").sendMessage(omsg.guild.members.get(entrada[1].slice(2, -1)));
         } /*else if(entrada[0] == "Felicitaciones," && entrada[2] == "pescaste:"){
             omsg.guild.channels.find("name", "log").sendMessage("ha pescado");
         }*/ else if (Math.random() < 0.11) {
