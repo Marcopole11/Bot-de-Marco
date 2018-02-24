bbb3cb7dea2237b1516c8ef0a7c75d2086fb7114
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-02-24 13:31:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,10 +13,9 @@ client.on('message', message => { //solo en chat de comandos
         } else if (message.content.startsWith(prefix + 'emote')) {
             //message.react(message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "Oland_flag"));
             //message.react(message.guild.emojis.find("name", "wagame"));
-            message.channel.send({embed}).then(m => {
+            message.channel.send("hola").then(m => {
                     m.react(m.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "Oland_flag"));
                     m.react(m.guild.emojis.find("name", "wagame"));
-
             }
         } else if (message.content.startsWith(prefix + 'testcomando')) {
             message.guild.channels.find("name", "canal-r37j").sendMessage('pong');
