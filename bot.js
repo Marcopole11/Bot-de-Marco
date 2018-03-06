116a7b39a2593cfe2864eb24a74b78c92ca98d2e
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-03-06 11:42:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -152,7 +152,7 @@ client.on('message', message => {
         
         message.channel.send(envio).then(m => {
             for(let opt = 0; opt < 3; opt++){
-                m.react(m.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "Oland_flag"));
+                m.react(m.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", emotinames[opt]));
             }
         });
     }
